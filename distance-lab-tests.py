886d7cac6995f4a4fe431c819cf1e059be4d389c
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2018-01-08 12:02:45 -0500

    adding tests

---
+++ distance-lab-tests.py
@@ -0,0 +1,33 @@
+import unittest
+from ipynb.fs.full.distancelab import (students,
+fred, daniel, street_distance, avenue_distance,
+distance_between_students_squared, distance, distance_with_student,
+distance_all, nearest_neighbors)
+
+class TestDistance(unittest.TestCase):
+    def test_street_distance(self):
+        self.assertEqual(street_distance(fred, daniel), 3)
+
+    def test_avenue_distance(self):
+        self.assertEqual(avenue_distance(fred, daniel), 3)
+
+    def test_distance_between_students_squared(self):
+        self.assertEqual(distance_between_students_squared(fred, daniel), 4)
+
+    def test_distance(self):
+        self.assertEqual(distance(fred, daniel), 2)
+
+    def test_distance_with_student(self):
+        self.assertEqual(distance_with_student(fred, daniel), {'avenue': 5, 'distance': 2.0, 'name': 'daniel', 'street': 1})
+
+    def test_distance_all(self):
+        self.assertEqual(distance_all(fred, students), [{'avenue': 5, 'distance': 2.0, 'name': 'daniel', 'street': 1},
+            {'avenue': 6, 'distance': 2.0, 'name': 'rachel', 'street': 2},
+            {'avenue': 10, 'distance': 1.4142135623730951, 'name': 'steven', 'street': 4}])
+
+    def test_nearest_neighbors(self):
+        self.assertEqual(nearest_neighbors(fred, students, 2), [{'avenue': 10, 'distance': 1.4142135623730951, 'name': 'steven', 'street': 4},
+            {'avenue': 5, 'distance': 2.0, 'name': 'daniel', 'street': 1}])
+
+if __name__ == '__main__':
+    unittest.main()
