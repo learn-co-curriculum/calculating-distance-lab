b7413026e1b31c2c1985108aa6f3305d98ff958b
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2018-02-17 14:44:42 -0500

    update tests

--- index-tests.py
+++ index-tests.py
@@ -21,13 +21,12 @@ class TestDistance(unittest.TestCase):
         self.assertEqual(distance_with_neighbor(fred, natalie), {'avenue': 5, 'distance': 4.123105625617661, 'name': 'Natalie', 'street': 4})
 
     def test_distance_all(self):
-        self.assertEqual(distance_all(fred, neighbors), [{'avenue': 4, 'distance': 0.0, 'name': 'Fred', 'street': 8},
- {'avenue': 1, 'distance': 4.242640687119285, 'name': 'Suzie', 'street': 11},
- {'avenue': 5, 'distance': 1.0, 'name': 'Bob', 'street': 8},
- {'avenue': 6, 'distance': 5.385164807134504, 'name': 'Edgar', 'street': 13},
- {'avenue': 3, 'distance': 2.23606797749979, 'name': 'Steven', 'street': 6},
- {'avenue': 5, 'distance': 4.123105625617661, 'name': 'Natalie', 'street': 4}])
+        self.assertEqual(distance_all(fred, neighbors), [[{'avenue': 1, 'distance': 4.242640687119285, 'name': 'Suzie', 'street': 11},
+             {'avenue': 5, 'distance': 1.0, 'name': 'Bob', 'street': 8},
+             {'avenue': 6, 'distance': 5.385164807134504, 'name': 'Edgar', 'street': 13},
+             {'avenue': 3, 'distance': 2.23606797749979, 'name': 'Steven', 'street': 6},
+             {'avenue': 5, 'distance': 4.123105625617661, 'name': 'Natalie', 'street': 4}])
 
     def test_nearest_neighbors(self):
-        self.assertEqual(nearest_neighbors(fred, neighbors, 2), [{'avenue': 4, 'distance': 0.0, 'name': 'Fred', 'street': 8},
-            {'avenue': 5, 'distance': 1.0, 'name': 'Bob', 'street': 8}])
+        self.assertEqual(nearest_neighbors(fred, neighbors, 2), [{'avenue': 5, 'distance': 1.0, 'name': 'Bob', 'street': 8},
+ {'avenue': 3, 'distance': 2.23606797749979, 'name': 'Steven', 'street': 6}])
